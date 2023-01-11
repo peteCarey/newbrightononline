ca3c3ae1da91a8d06b26b19cfb02769ae665921f
unknown <pete.carey@yahoo.co.uk>
2023-01-11 19:22:33 +0000

    initial commit

---
+++ power/_vti_cnf/AC_RunActiveContent.js
@@ -0,0 +1,8 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|02 Sep 2008 16:07:55 -0000
+vti_extenderversion:SR|5.0.2.4803
+vti_usagebymonth:UX|168 229 169 158 85 119 164 188 353 270 183 246 264
+vti_usagebyweek:UX|109 53 44 54 46
+vti_usagebyday:UX|12 14 20 20 12 19 12 8
+vti_usagelastupdated:TX|16 Apr 2011 22:59:59 -0000
+vti_usagetotalhits:IX|2596
